17879758b0f2060d37d3e1b251ba86b15ad981bf
jay nam <woalskdl@gmail.com>
2023-11-05 20:06:45 +0900

    일, 월, 년 별로 날짜 선택 (DateTimePicker 라이브러리) / 월별 이동

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { getCalendarColumns, getDayColor, getDayText } from './src/util';
 import dayjs from "dayjs"
 import Margin from './src/Margin';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 import { SimpleLineIcons } from '@expo/vector-icons'; 
 
@@ -44,15 +45,44 @@ export default function App() {
 
   const columns = getCalendarColumns(selectedDate);
 
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    setSelectedDate(dayjs(date));
+    hideDatePicker();
+  };
+
+  const onPressLeftArrow = () => {
+    const newSelectedDate = dayjs(selectedDate).subtract(1, 'month');
+    setSelectedDate(newSelectedDate);
+  }
+  
+  const onPressRightArrow = () => {
+    const newSelectedDate = dayjs(selectedDate).add(1, 'month');
+    setSelectedDate(newSelectedDate);
+  }
+
   const ListHeaderComponent = () => {
     const currentDateText = dayjs(selectedDate).format('YYYY.MM.DD.');
     return (
       <View>
         {/* < YYYY.MM.DD > */}
         <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-          <ArrowButton iconName={'arrow-left'} onPress={() => {}}/>
-          <Text style={{fontSize: 20, color: '#404040'}}>{currentDateText}</Text>
-          <ArrowButton iconName={'arrow-right'} onPress={() => {}}/>
+          <ArrowButton iconName={'arrow-left'} onPress={onPressLeftArrow}/>
+
+          <TouchableOpacity onPress={showDatePicker}>
+            <Text style={{fontSize: 20, color: '#404040'}}>{currentDateText}</Text>
+          </TouchableOpacity>
+
+          <ArrowButton iconName={'arrow-right'} onPress={onPressRightArrow}/>
         </View>
         <Margin height={15}/>
   
@@ -102,7 +132,7 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    console.log('selectedDate >> ', dayjs(selectedDate).format('YYYY.MM.DD'));
+    // console.log('selectedDate >> ', dayjs(selectedDate).format('YYYY.MM.DD'));
   }, [selectedDate]);
 
   return (
@@ -113,6 +143,13 @@ export default function App() {
         renderItem={renderItem}
         numColumns={7}
         ListHeaderComponent={ListHeaderComponent}
+      />
+
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
       />
     </SafeAreaView>
   );
