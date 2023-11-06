de4649b63a55f07fe68b6aea43e8c753b6719745
jay nam <woalskdl@gmail.com>
2023-11-06 22:44:46 +0900

    AsyncStorage 로 앱 리로드 후에도 저장한 데이터 불러오기

--- src/hook/use-todo-list.js
+++ src/hook/use-todo-list.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const defaultTodoList = [
   // {
@@ -22,9 +23,16 @@ const defaultTodoList = [
   // }
 ]
 
+const TODO_LIST_KEY = 'TODO_LIST_KEY';
+
 export const useTodoList = (selectedDate) => {
   const [todoList, setTodoList] = useState(defaultTodoList);
   const [input, setInput] = useState('');
+
+  const saveTodoList = (newTodoList) => {
+    setTodoList(newTodoList);
+    AsyncStorage.setItem(TODO_LIST_KEY, JSON.stringify(newTodoList));
+  }
 
   const addTodo = () => {
     if (!input)
@@ -43,24 +51,24 @@ export const useTodoList = (selectedDate) => {
       }
     ]
 
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   }
 
   const removeTodo = (todoId) => {
     const newTodoList = todoList.filter(todo => todo.id !== todoId);
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   }
 
   const toggleTodo = (todoId) => {
-    setTodoList(
-      todoList.map((todo) => {
-        if (todo.id === todoId)
-          return {
-            ...todo,
-            isSuccess : !todo.isSuccess
-          }
-      })
-    )
+    const newTodoList = todoList.map((todo) => {
+      if (todo.id === todoId)
+        return {
+          ...todo,
+          isSuccess : !todo.isSuccess
+        }
+    });
+
+    saveTodoList(newTodoList);
   }
 
   const resetInput = () => {
@@ -71,6 +79,16 @@ export const useTodoList = (selectedDate) => {
     const isSameDate = dayjs(todo.date).isSame(selectedDate, 'date');
     return isSameDate;
   })
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const result = await AsyncStorage.getItem(TODO_LIST_KEY);
+    if (result)
+      setTodoList(JSON.parse(result));
+  }
 
   return {
     todoList,
